39ed1c9c37fdea3caeaf727115aeef6f8d5e57df
shakib-anwar-nibir17 <nibirshakib@gmail.com>
2023-09-17 11:58:12 +0600

    problem 5 added

--- js-fundamentals/array/basic-problem-set1/problem1.js
+++ js-fundamentals/array/basic-problem-set1/problem1.js
@@ -3,8 +3,4 @@
 //my answer
 const array = []; //correct
 
-// Remove an element from an array by index.
-array2.pop(array2[2]);
-console.log(array2);
-
 // article for more knowledge: https://makersaid.com/declare-an-empty-array-javascript/
--- js-fundamentals/array/basic-problem-set1/problem5.js
+++ js-fundamentals/array/basic-problem-set1/problem5.js
@@ -1,3 +1,17 @@
 // Remove an element from an array by index.
-array2.pop(array2[2]);
-console.log(array2);
+
+const numbers = [10, 0, 1, 2, 3, 33, 4, 5, 6, 7, 8, 9, 11];
+
+// method 1: delete at index 0 by unshift
+
+numbers.shift();
+// method 2: delete at last index using pop
+numbers.pop();
+// method 3: delete at any index using splice()
+numbers.splice(4, 1);
+console.log(numbers); // output: [0,1,2,3,4,5,6,7,8,9]
+
+// learn about more methods:
+// https://www.freecodecamp.org/news/how-to-remove-an-element-from-a-javascript-array-removing-a-specific-item-in-js/#:~:text=You%20can%20remove%20the%20element,of%20the%20element%20to%20remove.&text=The%20splice%20method%20can%20accept%20many%20arguments.
+
+//https://www.slingacademy.com/article/how-to-remove-elements-from-an-array-in-javascript/
